8a38803659c32f7226df1de31567284ec4825710
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2018-05-21 18:27:53 -0500

    Refactor to use `pytest_collect_file` again since this is the only method that ensures all files are processed. Significantly a `py.Module` must be returned from this method in order for the file to be collected. The directories method skips the current directory resulting in test files not being collected.

--- conftest.py
+++ conftest.py
@@ -13,25 +13,30 @@ from isort.isort import SortImports
 
 TEST_FILE_PATTERNS = ['test_*.ipynb', '*_test.ipynb']
 
-paths_written = []
+py_paths_written = []
 
-def pytest_collect_directory(path, parent):
-    tests_by_pattern = [glob.glob(pathname=''.join([str(path), os.path.sep, pattern]), recursive=False)
-                        for pattern in TEST_FILE_PATTERNS]
-    tests = [test for tests_in_dir in tests_by_pattern for test in tests_in_dir]
-    for test in tests:
-        write_notebook_source(test)
+def pytest_collect_file(path, parent):
+    print('path file ', path)
+    for pattern in TEST_FILE_PATTERNS:
+        if glob.fnmatch.fnmatch(os.path.basename(path), pattern):
+            write_notebook_source(path)
+            py_path = os.path.splitext(path)[0] + '.py'
+            # see equivalently named method in:
+            #https://github.com/pytest-dev/pytest/blob/01d2d81d1f07607aaf26de566a9bd1f1c4cee6a9/_pytest/python.py
+            ihook = parent.session.gethookproxy(path)
+            return ihook.pytest_pycollect_makemodule(path=py_path, parent=parent)
         
 def pytest_unconfigure(config):
-    for path_written in paths_written:
-        os.remove(path_written)
-        print('Deleted file ', path_written)
-    paths_written.clear()
+    for py_path_written in py_paths_written:
+        os.remove(py_path_written)
+        print('Deleted file ', py_path_written)
+    py_paths_written.clear()
 
+# The code below needs a lot refactoring and checking.
 def write_notebook_source(path):
     nb = nbf.read(path, as_version=nbf.NO_CONVERT) # validate nbformat 4 only
         
-    # transform the input to executable Python
+    # transform the input to executable Python (clearly doesn't remove ipython code!!!)
     code = InteractiveShell.instance().input_transformer_manager.transform_cell(code_from_ipynb(nb))
         
     # add imports to code
@@ -54,7 +59,9 @@ def write_notebook_source(path):
             # if not common path bail!
             path_to_file = os.path.relpath(nb_path, common_path)
             path_to_file = os.path.dirname(path_to_file)
-            path_to_file = (path_to_file.replace(os.path.sep, '.') + '.' 
+            # replace path separator with . following rules:
+            # https://docs.pytest.org/en/latest/goodpractices.html
+            path_to_file = (path_to_file.replace(os.path.sep, '.') + '.'
                 + os.path.split(nb_path)[1])
             path_to_file, _ = os.path.splitext(path_to_file)
             import_statement = ('from ' + path_to_file + ' import * ')
@@ -67,9 +74,8 @@ def write_notebook_source(path):
     code = filter_ast(ast.parse(code)) #code_from_ipynb(nb)
         
     # write the file
-    filename, _ = os.path.splitext(path)
-    filename = filename + '.py'
-    with open(filename, 'w', encoding='utf-8') as py_file:
+    py_path = os.path.splitext(path)[0] + '.py'
+    with open(py_path, 'w', encoding='utf-8') as py_file:
         py_file.write(to_source(code))
-        print('Written file ', filename)
-        paths_written.append(filename)
+        print('Written file ', py_path)
+        py_paths_written.append(py_path)
